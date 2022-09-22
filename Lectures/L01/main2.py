1507681175f4ba98f18cc117abd0eb28b59365a3
Andrii Krenevych <akrenevych@gmail.com>
2022-09-22 08:37:54 +0300

    Лекція 1

---
+++ Lectures/L01/main2.py
@@ -0,0 +1,17 @@
+# print("Hello from main 2")
+# print("Hello from main 2")
+# print("Hello from main 2")
+
+# x = input("Введіть щось ")  # В цьому рядку коду, я вводжу змінну х
+
+# a = float(input("введіть перший катет")) # int - перетворення до цілого, float - до дійсного
+# b = float(input("введіть другий катет"))
+a = 3
+b = 4
+c = (a**2 + b**2) **0.5
+print("Гіпотенуза = ", c)
+pass
+
+
+
+
