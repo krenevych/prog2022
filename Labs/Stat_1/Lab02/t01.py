97f58b4120a8f8b433563d1a1cc13a002f7a4944
Andrii Krenevych <akrenevych@gmail.com>
2022-09-29 10:55:42 +0300

    1

---
+++ Labs/Stat_1/Lab02/t01.py
@@ -0,0 +1,16 @@
+# True або False
+
+n = 67
+# n = int(input("n = "))
+
+condition_a = n % 2 == 0
+print("a)", condition_a)
+
+condition_b = n % 10 == 0
+print("b)", condition_b)
+
+od = n % 10
+de = n // 10
+condition_c = od + de > 9
+print("c)", condition_c)
+pass
