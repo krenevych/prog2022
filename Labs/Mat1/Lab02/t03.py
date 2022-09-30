665a5992189794d17deb342241c0b0af36ab23ae
Andrii Krenevych <akrenevych@gmail.com>
2022-09-30 09:49:59 +0300

    3

---
+++ Labs/Mat1/Lab02/t03.py
@@ -0,0 +1,12 @@
+
+n = int(input())
+
+# condition_1 = n % 2 == 1
+# condition_2 = 99 < n < 1000
+#
+# condition = condition_1 or condition_2
+
+if n % 2 == 1 or 99 < n < 1000:
+    print("YES")
+else:
+    print("NO")
