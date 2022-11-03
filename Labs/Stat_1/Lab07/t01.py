4ccf8c05984fd5779677375abbc86222709c0c52
Andrii Krenevych <akrenevych@gmail.com>
2022-11-03 16:24:59 +0200

    33

---
+++ Labs/Stat_1/Lab07/t01.py
@@ -0,0 +1,13 @@
+def isPrime(k):
+    for i in range(2, k):
+    # for i in range(2, int(k) ** 0.5 + 1): # Оптимізація
+        if k % i == 0:
+            return False
+    return True
+
+
+####### Main program #############
+n = int(input("n = "))
+for i in range(n, 2 * n - 1):
+    if isPrime(i) and isPrime(i + 2):
+        print(i, i + 2)
