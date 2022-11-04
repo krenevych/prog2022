f29ea8dca0f6b0b1318baeaec3d9db31def09625
Andrii Krenevych <krenevych@knu.ua>
2022-11-04 09:43:09 +0200

    Create t02.py

---
+++ Labs/Mat1/Lab07/t02.py
@@ -0,0 +1,40 @@
+# a) повними квадратами;
+def isSquare(k):
+    return int(k**0.5) ** 2 == k
+
+# b) степенями п’ятірки;
+def isPow5(k):
+    # k = 125
+    while k > 1:
+        if k % 5 != 0:
+            return False
+        k = k // 5
+    return True
+
+# c) простими числами.
+def prime(k):
+    for i in range(2, k):
+        if k % i == 0:
+            return False
+    return True
+
+####### Main ########
+# Дано натуральне число n та послідовність натуральних чисел
+# a_1,a_2,...a_n.
+
+squares = []
+pow5s = []
+primes = []
+n = int(input())
+for i in range(n):
+    a = int(input())
+    if isSquare(a):
+        squares.append(a)
+    if isPow5(a):
+        pow5s.append(a)
+    if prime(a):
+        primes.append(a)
+
+print("Повні квадрати: ", *squares)
+print("Степені 5: ", *pow5s)
+print("Прості: ", *primes)
