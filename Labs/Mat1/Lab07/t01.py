82f08d8d59f9083e21945a7188c3d47c9715d35e
Andrii Krenevych <krenevych@knu.ua>
2022-11-04 09:19:30 +0200

    Create t01.py

---
+++ Labs/Mat1/Lab07/t01.py
@@ -0,0 +1,12 @@
+def prime(k):
+    for i in range(2, k):
+        if k % i == 0:
+            return False
+    return True
+
+
+##### Main #######
+n = int(input())
+for i in range(n, 2*n-1):
+    if prime(i) and prime(i+2):
+        print(i, i + 2)
