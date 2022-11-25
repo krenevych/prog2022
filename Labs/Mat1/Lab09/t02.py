1e5ccd6fee04c5bd154b4b332f31cc6ca3640397
Andrii Krenevych <akrenevych@gmail.com>
2022-11-25 09:10:46 +0200

    333

---
+++ Labs/Mat1/Lab09/t02.py
@@ -0,0 +1,21 @@
+n = int(input())
+elems = [int(e) for e in input().split()]
+
+# print(elems)
+
+# словник частот входження чисел.
+freq = {}
+for el in elems:
+    if el in freq:
+        continue
+
+    freq[el] = elems.count(el)
+
+# print(freq)
+n_2 = n // 2
+for key, val in freq.items():
+    if val > n_2:
+        print(key)
+        break
+else:
+    print(-1)
