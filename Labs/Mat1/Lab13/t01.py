07170e2c0242633396a78d26d1679f8bef5eb920
Andrii Krenevych <akrenevych@gmail.com>
2023-01-23 12:50:07 +0200

    322

--- Labs/Mat1/Lab13/t01.py
+++ Labs/Mat1/Lab13/t01.py
@@ -1,1 +1,8 @@
-print("Hello")
+def readFile(fileName):
+    with open(fileName, encoding="utf-8") as f:
+        for line in f:
+            print(line.strip())
+
+
+if __name__ == '__main__':
+    readFile("input01.txt")
