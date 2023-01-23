5f8f68049ae66bc0112d3e43c28f8f87724cdd11
Andrii Krenevych <akrenevych@gmail.com>
2023-01-23 12:56:47 +0200

    322

--- Labs/Mat1/Lab13/t01.py
+++ Labs/Mat1/Lab13/t01.py
@@ -3,6 +3,14 @@ def readFile(fileName):
         for line in f:
             print(line.strip())
 
+def readFile60(fileName):
+    with open(fileName, encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) > 60:
+                print(line)
+
+
 
 if __name__ == '__main__':
-    readFile("input01.txt")
+    readFile60("input01.txt")
