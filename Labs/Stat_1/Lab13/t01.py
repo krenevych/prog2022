413ff7792ab82c4ad776027b6b0f4e3042ebc610
Andrii Krenevych <akrenevych@gmail.com>
2023-01-24 11:21:24 +0200

    1111

--- Labs/Stat_1/Lab13/t01.py
+++ Labs/Stat_1/Lab13/t01.py
@@ -1,11 +1,37 @@
 def readFile(file_name):
-    with open(file_name, "rt",  encoding='utf-8') as f:
+    with open(file_name, "rt", encoding='utf-8') as f:
         for line in f:
             line = line.strip()
             print(line)
 
 
+def printMore60(file_name):
+    with open(file_name, "rt", encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if len(line) > 60:
+                print(line)
 
 
-readFile("input01.txt")
+def countEmpty(file_name):
+    with open(file_name, "rt", encoding='utf-8') as f:
+        return [line.strip() for line in f].count("")
+
+
+def findMaxLen(file_name):
+    maxLine = ""
+    with open(file_name, "rt", encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if len(line) > len(maxLine):
+                maxLine = line
+
+    return maxLine
+
+
+# readFile("input01.txt")
+# printMore60("input01.txt")
+# print(countEmpty("input01.txt"))
+print(findMaxLen("input01.txt"))
+
 print("============")
