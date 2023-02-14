5728d03e7c1256421ecf3ddd15644b1f36b9d2d5
Andrii Krenevych <akrenevych@gmail.com>
2023-02-14 16:49:15 +0200

    1111

--- Labs/Mat1/OOP01/QuadraticEquation.py
+++ Labs/Mat1/OOP01/QuadraticEquation.py
@@ -1,25 +1,33 @@
 class QuadraticEquation:
 
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def __init__(self, a, b=None, c=None):
+        if isinstance(a, QuadraticEquation): # a - це квадратне рівняння, тобто екземпляр класу QuadraticEquation
+            self.__a = a.__a
+            self.__b = a.__b
+            self.__c = a.__c
+        else:
+            self.__a = a
+            self.__b = b
+            self.__c = c
+
+    def clone(self):
+        return QuadraticEquation(self.__a, self.__b, self.__c)
 
     def show(self):
         print(self)
 
     def __str__(self):
-        return f"{self.a} x**2 + {self.b}x + {self.c} = 0"
+        return f"{id(self)}: {self.__a} x**2 + {self.__b}x + {self.__c} = 0"
 
     def discriminant(self):
-        return self.b ** 2 - 4 * self.a * self.c
+        return self.__b ** 2 - 4 * self.__a * self.__c
 
     def solutions(self):
-        if self.a == 0:  # рівняння вироджується в лінійне
-            if self.b != 0:  # bx + c = 0
-                return -self.c / self.b,  # кортеж з одного елементу
+        if self.__a == 0:  # рівняння вироджується в лінійне
+            if self.__b != 0:  # bx + c = 0
+                return -self.__c / self.__b,  # кортеж з одного елементу
             else:  # c = 0
-                if self.c == 0:  # 0 = 0
+                if self.__c == 0:  # 0 = 0
                     return "Inf"  # стала "Inf" означає, що в нас нескінченна кількість розв'язків
                 else:  # 3 = 0
                     return ()  # розв'язків немає
@@ -28,11 +36,11 @@ class QuadraticEquation:
             if d < 0:  # розв'язків немає
                 return ()
             elif d == 0:                         # один розв'язок
-                return -self.b / (2 * self.a),
+                return -self.__b / (2 * self.__a),
             else:                                # d > 0, два розв'язки
                 d_2 = d ** 0.5
-                x1 = (-self.b - d_2) / (2 * self.a)
-                x2 = (-self.b + d_2) / (2 * self.a)
+                x1 = (-self.__b - d_2) / (2 * self.__a)
+                x2 = (-self.__b + d_2) / (2 * self.__a)
                 return x1, x2
 
 
@@ -41,22 +49,9 @@ if __name__ == '__main__':
     print(eq1)
     print("solutions = ", eq1.solutions())
 
-    eq2 = QuadraticEquation(1, 4, 4)
-    print(eq2)
-    print("solutions = ", eq2.solutions())
+    copy_eq1 = QuadraticEquation(eq1)
+    # copy_eq1 = eq1.clone()
+    copy_eq1._QuadraticEquation__a = 0
+    print(copy_eq1)
+    print("solutions = ", copy_eq1.solutions())
 
-    eq3 = QuadraticEquation(1, -3, 2)
-    eq3.show()
-    print("solutions = ", eq3.solutions())
-
-    lin_eq1 = QuadraticEquation(0, -3, 2)
-    lin_eq1.show()
-    print("solutions = ", lin_eq1.solutions())
-
-    lin_eq2 = QuadraticEquation(0, 0, 2)
-    lin_eq2.show()
-    print("solutions = ", lin_eq2.solutions())
-
-    lin_eq3 = QuadraticEquation(0, 0, 0)
-    lin_eq3.show()
-    print("solutions = ", lin_eq3.solutions())
--- Labs/Mat1/OOP01/main.py
+++ Labs/Mat1/OOP01/main.py
@@ -15,8 +15,68 @@ def readQuadraticEquation(file):
 
     return equations
 
+    # Для заданого набору квадратних рівнянь, покажіть ті з них, що:
+equations_with_0_solutions = []    # •	не мають розв’язків;
+equations_with_1_solutions = []    # •	мають один розв’язок;
+equations_with_2_solutions = []    # •	мають два розв’язки;
+equations_with_Inf_solutions = []    # •	мають нескінченну кількість розв’язків.
+
 
 if __name__ == '__main__':
     equations = readQuadraticEquation("input01.txt")
     for equation in equations:
+        solutions = equation.solutions()
+        if solutions == "Inf":
+            equations_with_Inf_solutions.append(equation)
+        elif len(solutions) == 0:
+            equations_with_0_solutions.append(equation)
+        elif len(solutions) == 1:
+            equations_with_1_solutions.append(equation)
+        else:
+            equations_with_2_solutions.append(equation)
+
+    print("Рівняння, що не мають розв'язків: ")
+    for equation in equations_with_0_solutions:
         print(f"solution of equation {equation} is {equation.solutions()}")
+
+    print("=========================================")
+    print("Рівняння, що мають один розв'язок: ")
+    for equation in equations_with_1_solutions:
+        print(f"solution of equation {equation} is {equation.solutions()}")
+
+    print("=========================================")
+    print("Рівняння, що мають два розв'язки: ")
+    for equation in equations_with_2_solutions:
+        print(f"solution of equation {equation} is {equation.solutions()}")
+
+    print("=========================================")
+    print("Рівняння, що мають нескінченну кількість розв'язків: ")
+    for equation in equations_with_Inf_solutions:
+        print(f"solution of equation {equation} is {equation.solutions()}")
+
+    equation_with_min_solution = equations_with_1_solutions[0]
+    min_sol = equation_with_min_solution.solutions()[0]
+
+    equation_with_max_solution = equations_with_1_solutions[0]
+    max_solution = equation_with_min_solution.solutions()[0]
+
+    for equation in equations_with_1_solutions[1:]:
+        solutions = equation.solutions()
+        if solutions[0] < min_sol:
+            equation_with_min_solution = equation
+            min_sol = solutions[0]
+
+        if solutions[0] > max_solution:
+            equation_with_max_solution = equation
+            max_solution = solutions[0]
+
+    print("==================================")
+    print(f"з усіх рівнянь, що мають рівно один розв’язок,"
+          f" рівняння {equation_with_min_solution} "
+          f"має найменший розв'язок {min_sol} ")
+
+    print("==================================")
+    print(f"з усіх рівнянь, що мають рівно один розв’язок,"
+          f" рівняння {equation_with_max_solution} "
+          f"має найбільший розв'язок {max_solution} ")
+
